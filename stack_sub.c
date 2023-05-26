dce7b381b8550ab97e7a22e6be132e79f3800659
Gold Israel <techiegld101@gmail.com>
2023-05-26 14:29:35 -0700

    Implement sub opcode for stack subtraction

---
+++ stack_sub.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * sub - subtracts the top element from the second top element
+ * @stack: pointer to the stack
+ * @line_number: Line number
+ *
+ * Return: void
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L:%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
