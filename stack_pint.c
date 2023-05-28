fd6ef7c99241ca554801b8382b3942c0cf9fbe97
Gold Israel <techiegld101@gmail.com>
2023-05-28 12:32:41 -0700

    Implement pint opcode

---
+++ stack_pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pint - print value at top of stack
+ * @stack: stack
+ * @line_number: line_number
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
