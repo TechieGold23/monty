a064d8a39d1dc607a3bbdb4dbc7759638a5384ec
Gold Israel <techiegld101@gmail.com>
2023-05-28 12:49:53 -0700

    Improve code

--- stack_mul.c
+++ stack_mul.c
@@ -10,13 +10,14 @@
 
 void mul(stack_t **stack, unsigned int line_number)
 {
+	int product;
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	int product = (*stack)->next->n * (*stack)->n;
+	product = (*stack)->next->n * (*stack)->n;
 
 	pop(stack, line_number);
 	(*stack)->n = product;
