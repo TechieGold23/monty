ad56beec844c452f5ce8e327c6036438f151108c
Gold Israel <techiegld101@gmail.com>
2023-05-28 12:37:27 -0700

    Implement add opcode

---
+++ stack_add.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * add - addthe top 2 elements of the stack
+ * @stack: stack
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
